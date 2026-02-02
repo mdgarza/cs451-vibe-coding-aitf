08ba4c0463120662704b61ff3e2eadc93df3f00a
mdgarza <raijox989@outlook.com>
2026-02-02 00:02:38 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const path = require("path");
 
+const app = express();
+const PORT = 3000;
+
+// tell Express to use EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// basic route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// start the server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
